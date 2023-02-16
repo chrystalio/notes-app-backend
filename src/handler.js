9dcedf840c549522f5e3efe7f38c3dc0d0ddd18e
chrystalio <chrystalio.k@outlook.com>
2023-02-17 01:04:55 +0700

    Add handler DeleteNoteByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -63,7 +63,7 @@ const getNoteByIdHandler = (request, h) => {
     });
     response.code(404);
     return response;
-}
+};
 
 const editNoteByIdHandler = (request, h) => {
     const { id } = request.params;
@@ -91,4 +91,27 @@ const editNoteByIdHandler = (request, h) => {
     }
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1){
+        notes.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan Berhasil Dihapus',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
